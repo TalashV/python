1278639da2396d816996aff2fcf8c91d4f8e8ed5
Vladimir Talashkevich <talashv@MacBook-Air-Vladimir.local>
2022-03-20 23:33:03 +0300

    as

--- new.py
+++ new.py
@@ -1,1 +1,8 @@
-s
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('https://www.meshkovbrest.by/files/editor_Заявление_на_частичный_возврат_Вардар20221.doc')
+.encode
+
+for line in fhand :
+    print (line.decode().strip())
+
